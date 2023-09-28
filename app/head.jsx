1d0ab5703f81aa36bd6c0650c3d385b8d724f32b
ksachikonye <72452810+ksachikonye@users.noreply.github.com>
2023-09-28 14:36:41 +0000

    Update 33 files

--- app/head.jsx
+++ app/head.jsx
@@ -55,6 +55,8 @@ export default function Head() {
         Be sure validate your Twitter card markup on the documentation site. */}
       <meta name='twitter:card' content='summary' />
       <meta name='twitter:site' content={twitter} />
+ 
+
     </>
   )
 }
